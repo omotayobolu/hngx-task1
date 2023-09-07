b00797d6d7dc152bf0b274393c6cbf519372471d
Boluwatife <omotayodaniel10@gmail.com>
2023-09-07 17:24:01 +0100

    done

---
+++ script.js
@@ -0,0 +1,23 @@
+const day = document.getElementById("currentDay");
+const time = document.getElementById("currentUTCTime");
+
+weekDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const today = new Date();
+
+const todayDay = today.getDay();
+day.innerText = weekDays[todayDay];
+
+const UTCTime = `${today.getUTCHours()}:${today
+  .getUTCMinutes()
+  .toString()
+  .padStart(2, "0")}:${today.getUTCSeconds()} `;
+time.innerHTML = UTCTime;
